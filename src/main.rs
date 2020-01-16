99b4b07f923277190c7626379e5266762d2c5eff
kirfed <kirfed@gmail.com>
2020-01-16 11:18:01 +0300

    rand tree 0.2.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use glam::Vec2;
+use hex_field::algo::rand_tree::{RandHexTree, MappedTree};
 use hex_field::*;
 use plotters::prelude::*;
+use rand::prelude::*;
 
 trait ToLine {
     fn to_line(&self) -> Vec<(f32, f32)>;
@@ -8,54 +11,50 @@ trait ToLine {
 
 impl ToLine for Hex {
     fn to_line(&self) -> Vec<(f32, f32)> {
-        vec![
-            self.top_left(),
-            self.top_right(),
-            self.right(),
-            self.bot_right(),
-            self.bot_left(),
-            self.left(),
-            self.top_left(),
-        ]
+        let mut v: Vec<(f32, f32)> = self.vertices().map(|v| v.into()).collect();
+        v.push(self.left_top_vert().into());
+        v
     }
 
     fn link_to(&self, other: &Self) -> Vec<(f32, f32)> {
-        vec![self.center(), other.center()]
+        vec![self.center().into(), other.center().into()]
     }
 }
 
 fn draw_node(
     chart: &mut ChartContext<BitMapBackend, RangedCoord<RangedCoordf32, RangedCoordf32>>,
-    node: &algo::rand_tree::Node<Hex>,
+    tree: &MappedTree<Hex>,
+    node: &Hex,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    chart.draw_series(LineSeries::new(node.data.to_line(), &BLUE))?;
-    for n in node.iter() {
-        chart.draw_series(LineSeries::new(node.data.link_to(&n.data), &RED))?;
-        draw_node(chart, n)?;
+    chart.draw_series(LineSeries::new(node.to_line(), &BLUE))?;
+    let children = tree.children(node);
+    for child in children {
+        draw_node(chart, tree, child)?;
+        chart.draw_series(LineSeries::new(node.link_to(child), &RED))?;
     }
 
     Ok(())
 }
 
-fn draw_line(
-    chart: &mut ChartContext<BitMapBackend, RangedCoord<RangedCoordf32, RangedCoordf32>>,
-    p1: (f32, f32),
-    p2: (f32, f32),
-) -> Result<(), Box<dyn std::error::Error>> {
-    chart.draw_series(LineSeries::new(vec![p1, p2], &BLUE))?;
-    Ok(())
-}
-
-fn draw_lines_to_vertices(
-    chart: &mut ChartContext<BitMapBackend, RangedCoord<RangedCoordf32, RangedCoordf32>>,
-    p: (f32, f32),
-    hex: &Hex,
-) -> Result<(), Box<dyn std::error::Error>> {
-    for v in &hex.points() {
-        draw_line(chart, *v, p)?;
-    }
-    Ok(())
-}
+// fn draw_line(
+//     chart: &mut ChartContext<BitMapBackend, RangedCoord<RangedCoordf32, RangedCoordf32>>,
+//     p1: (f32, f32),
+//     p2: (f32, f32),
+// ) -> Result<(), Box<dyn std::error::Error>> {
+//     chart.draw_series(LineSeries::new(vec![p1, p2], &BLUE))?;
+//     Ok(())
+// }
+//
+// fn draw_lines_to_vertices(
+//     chart: &mut ChartContext<BitMapBackend, RangedCoord<RangedCoordf32, RangedCoordf32>>,
+//     p: (f32, f32),
+//     hex: &Hex,
+// ) -> Result<(), Box<dyn std::error::Error>> {
+//     for v in &hex.points() {
+//         draw_line(chart, *v, p)?;
+//     }
+//     Ok(())
+// }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new("twoscale.png", (1920, 1440)).into_drawing_area();
@@ -72,19 +71,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     chart.configure_mesh().draw()?;
 
+
+    let center = Vec2::new(ax / 2f32, ax / 2f32);
     let conf = Config {
-        width: 400f32,
-        height: 200f32,
-        offset_x: ax / 2f32,
-        offset_y: ax / 2f32,
+        hex_size: Vec2::new(20f32, 16f32),
+        offset: center,
     };
     let hf = HexField::new(conf);
-    let tree = algo::rand_tree::generate(&hf, 5, (ax / 2f32, ax / 2f32), 0.6f32, 1);
+    let hex = Hex::new(
+        hf.hex_center_by_containing_point(center),
+        hf.hex_size(),
+    );
 
-    draw_node(&mut chart, &tree)?;
+    let mut tree = algo::rand_tree::RandHexTree::new(hex, 0.4f64, StdRng::seed_from_u64(4));
+    tree.add_hexes(540);
+    println!("Calculated!!!");
+    draw_node(&mut chart, &tree.tree(), &tree.tree().root().unwrap())?;
 
-    let c = tree.root().data.clone();
-    let (t, tr) = (c.top_hex(), c.top_right_hex());
+    // let c = tree.root().data.clone();
+    // let (t, tr) = (c.top_hex(), c.top_right_hex());
     // chart.draw_series(LineSeries::new(
     //     (0..=200).map(|t| {
     //         let angle = (t as f32) / 200f32 * 2f32 * std::f32::consts::PI;
